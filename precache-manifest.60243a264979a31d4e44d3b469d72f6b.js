525598a7e8761ef49ec6e6acc6832ebc39905119
Claire McCrea <claire.n.mccrea@gmail.com>
2019-02-11 16:32:28 -0700

    Updates

---
+++ precache-manifest.60243a264979a31d4e44d3b469d72f6b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "6b5ee608098d11b0969207e6c92532d5",
+    "url": "/RonSwansonQuoteGenerator/static/media/German-plaid.6b5ee608.jpeg"
+  },
+  {
+    "revision": "df5b8bb1023934448856",
+    "url": "/RonSwansonQuoteGenerator/static/js/runtime~main.df5b8bb1.js"
+  },
+  {
+    "revision": "16b1c85ed2678fdd4b17",
+    "url": "/RonSwansonQuoteGenerator/static/js/main.16b1c85e.chunk.js"
+  },
+  {
+    "revision": "76a6f8204d056adcd8d1",
+    "url": "/RonSwansonQuoteGenerator/static/js/1.76a6f820.chunk.js"
+  },
+  {
+    "revision": "16b1c85ed2678fdd4b17",
+    "url": "/RonSwansonQuoteGenerator/static/css/main.ed67fa8d.chunk.css"
+  },
+  {
+    "revision": "76a6f8204d056adcd8d1",
+    "url": "/RonSwansonQuoteGenerator/static/css/1.6f803018.chunk.css"
+  },
+  {
+    "revision": "555f5a65210db2e28bb080ce2755de37",
+    "url": "/RonSwansonQuoteGenerator/index.html"
+  }
+];
